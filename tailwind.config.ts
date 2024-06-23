5779b316a902b834443aa878743f4d9d35b8b9ce
aronaldo9 <aaronpesqueirafarina@gmail.com>
2024-06-23 09:16:05 +0200

    empezando portfolio

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,13 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        secondary: "#00ced1",
+        darkBg: "#131424",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-cover":
+          "linear-gradient(90.21deg, rgba(19, 20, 36, 0.5) -5.91%, rgba(19, 20, 36, 0.5) 111.58%)",
       },
     },
   },
