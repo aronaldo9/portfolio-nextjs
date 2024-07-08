3fa9afc9403cc3b7fa53e5e967dc3cfec1174729
aronaldo9 <aaronpesqueirafarina@gmail.com>
2024-07-08 21:05:10 +0200

    cambiando imágenes

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -7,9 +7,9 @@ import { TypeAnimation } from "react-type-animation";
 const Introduction = () => {
   return (
     <div className="z-20 w-full bg-darkBg/60">
-      <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
+      <div className="z-20 grid items-center h-full p-6 my-4 py-20 md:py-0 md:grid-cols-2">
         <Image
-          src="/home-45.png"
+          src="/aaron.png"
           priority
           width="700"
           height="700"
